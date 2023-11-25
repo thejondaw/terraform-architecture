55d3e462411a64975c766741122445a8b7ae9220
Jon Daw <142529123+thejondaw@users.noreply.github.com>
2023-11-25 15:33:51 -0600

    db

--- rds/main.tf
+++ rds/main.tf
@@ -15,3 +15,16 @@ resource "aws_db_subnet_group" "default" {
   name       = "main"
   subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "wordpress"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name  = aws_db_subnet_group.default.name
+}
