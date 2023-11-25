ff3c2b82de4e0955b10acf9d53c4294b88992228
Jon Daw <142529123+thejondaw@users.noreply.github.com>
2023-11-25 15:29:26 -0600

    sdfsd

--- rds/main.tf
+++ rds/main.tf
@@ -13,7 +13,5 @@ data "terraform_remote_state" "vpc" {
 # Subnet Group with Private Subnets
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = [
-    data.terraform_remote_state.vpc.outputs.private_subnets
-  ]
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
